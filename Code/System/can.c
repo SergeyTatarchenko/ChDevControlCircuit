6c3cb584a8d0e934381a21a76b23474130578079
SergeyTatarchenko <zlojdigger@gmail.com>
2019-05-04 15:14:01 +0300

    - can work

--- Code/System/can.c
+++ Code/System/can.c
@@ -2,12 +2,12 @@
 * File Name          : can.c
 * Author             : Tatarchenko S.
 * Version            : v 1.0
-* Description        : CAN 2.0b bus module for stm32f1x
+* Description        : bxCAN  bus module for stm32f1x
 *************************************************/
 #include "can.h"
 
 /*************************************************
-Init CAN
+                   Init CAN
 *************************************************/
 void CAN_init(){
 	
@@ -29,16 +29,46 @@ void CAN_init(){
 	GPIOA->CRH |= GPIO_CRH_CNF12_1;					 //PA12
 	
 	/*init can module*/
-	
+	can_set_mode(can_mode_loopback);
 }
 
 /*************************************************
-set can baudrate
+ global CAN and baudrate config 
 *************************************************/
-static void can_set_baudrate(){
+static void can_set_mode(int mode){
+	
+	/*enter init mode*/
+	CAN1->MCR |= CAN_MCR_INRQ;
+	while((CAN1->MSR & CAN_MSR_INAK)==0);
+	/*
+	Time Triggered Communication Mode disable;
+	Set the no automatic retransmission disable;
+	Set the receive FIFO locked mode disable;
+	*/
+	CAN1->MCR &=~(CAN_MCR_TTCM|CAN_MCR_NART|CAN_MCR_RFLM);
+	/*
+	Set the automatic bus-off management enable;
+	Set the automatic wake-up mode enable;
+	Set the transmit FIFO priority enable;
+	*/
+	CAN1->MCR |=(CAN_MCR_TTCM|CAN_MCR_AWUM|CAN_MCR_TXFP);
+	/*set can mode*/
+	CAN1->BTR &= ~(CAN_BTR_BRP|CAN_BTR_TS1|CAN_BTR_TS2|CAN_BTR_SJW);
+	
+	CAN1->BTR |= ((uint32_t)mode<<30)|
+				 ((uint32_t)(can_TS1-1)<<16)|
+				 ((uint32_t)(can_TS2-1)<<20)|
+				  (uint32_t)(can_BRR-1);
+		
+}
+
+
+static void can_id_filter_config(){
+	
 	
 }
 
-static void can_set_mode(){
-	
-}
+
+
+
+
--- Code/System/can.h
+++ Code/System/can.h
@@ -11,18 +11,68 @@
 #include "system_stm32f10x.h"
 #include "string.h"
 
+
+
+
 /*-----------local define-----------------------*/
-typedef enum{
-	baud_250 = 250,
-	baud_500 = 500,
-	baud_1000 = 1000,
-}can_baudrate;
+/*define can clock in MHz*/
+#define CAN_CLOCK	36
+/*----------------------------------------------*/
+typedef struct {
 	
+  uint32_t StdId; 
+  uint32_t ExtId;  
+  uint8_t IDE;     
+  uint8_t RTR;    
+  uint8_t DLC;    
+  uint8_t Data[8];
+
+} CanTxMsg;
+
+typedef struct{
+	
+  uint32_t StdId; 
+  uint32_t ExtId; 
+  uint8_t IDE;    
+  uint8_t RTR;    
+  uint8_t DLC;    
+  uint8_t Data[8];
+  uint8_t FMI;  
+} CanRxMsg;
+
+
+
+#define	can_mode_normal				0
+#define	can_mode_loopback			1
+#define	can_mode_silent				2
+#define	can_mode_silent_loopback	3
+
+/*
+	values of BRR, TS1 and TS2 calc in	
+	http://www.bittiming.can-wiki.info/ 
+*/
+
+#if CAN_CLOCK == 16
+	/*can baudrate 250 kb/s ; CAN clock 16 Mhz*/ 	
+	#define can_BRR	4
+	#define can_TS1	13
+	#define can_TS2	2
+
+#elif CAN_CLOCK == 36
+	/*can baudrate 250 kb/s ; CAN clock 36 Mhz*/ 	
+	#define can_BRR	9
+	#define can_TS1	13
+	#define can_TS2	2
+
+#endif
+
+/*----------------------------------------------*/
+
 /*-----------global function prototypes---------*/
 extern void CAN_init(void);
 /*-----------local function prototypes----------*/
 
 
-static void can_set_baudrate(void);
-static void can_set_mode(void);
+static void can_id_filter_config(void);
+static void can_set_mode(int mode);
 #endif
