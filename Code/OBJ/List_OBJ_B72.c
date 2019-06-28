ccdce0d62b0ec9f5686006edd52787e3aa8ae81a
SergeyTatarchenko <zlojdigger@gmail.com>
2019-06-28 16:25:27 +0300

    - fix adc

--- Code/OBJ/List_OBJ_B72.c
+++ Code/OBJ/List_OBJ_B72.c
@@ -20,45 +20,52 @@ void ADC_Handler(OBJ_STRUCT *obj){
 	static const int load = 10;
 	static int indication;
 	
-	/*input voltage DVL 1000 - 50uA/V */
-	indication =((3300 - adc_val->CH1_ADC)/load)*20;
-	if(indication > 0){
-		this_obj(IND_obj_aINV)->obj_value = indication;
-	}else{
-		this_obj(IND_obj_aINV)->obj_value = 0;
-	}
-	
-	/*input current LAC 300 - 33uA/A */
-	indication =((3300 - adc_val->CH2_ADC)/load)*30;
-	if(indication > 0){
-		this_obj(IND_obj_aINC)->obj_value = indication;
-	}else{
-		this_obj(IND_obj_aINC)->obj_value = 0;
-	}
-	
-	/*output voltage DVL 1000 - 50uA/V */
-	indication =((3300 - adc_val->CH3_ADC)/load)*20;
-	if(indication > 0){
-		this_obj(IND_obj_aOUTV)->obj_value = indication;
-	}else{
-		this_obj(IND_obj_aOUTV)->obj_value = 0;
-	}
-	
-	/*output current LAC 300 - 33uA/A */
-	indication =((3300 - adc_val->CH4_ADC)/load)*30;
-	if(indication > 0){
-		this_obj(IND_obj_aOUTC)->obj_value = indication;
-	}else{
-		this_obj(IND_obj_aOUTC)->obj_value = 0;
-	}
-	
-	/*inductor voltage DVL 1000 - 50uA/V */
-	indication =((3300 - adc_val->CH5_ADC)/load)*20;
-	if(indication > 0){
-		this_obj(IND_obj_aDRV)->obj_value = indication;
-	}else{
-		this_obj(IND_obj_aDRV)->obj_value = 0;
-	}
+	this_obj(IND_obj_aINV)->obj_value = adc_val->CH1_ADC;
+	this_obj(IND_obj_aINC)->obj_value = adc_val->CH2_ADC;
+	this_obj(IND_obj_aOUTV)->obj_value = adc_val->CH3_ADC;
+	this_obj(IND_obj_aOUTC)->obj_value = adc_val->CH4_ADC;
+	this_obj(IND_obj_aDRV)->obj_value = adc_val->CH5_ADC;
+	this_obj(IND_obj_aDRC)->obj_value = adc_val->CH6_ADC;
+
+	//	/*input voltage DVL 1000 - 50uA/V */
+//	indication =((3300 - adc_val->CH1_ADC)/load)*20;
+//	if(indication > 0){
+//		this_obj(IND_obj_aINV)->obj_value = indication;
+//	}else{
+//		this_obj(IND_obj_aINV)->obj_value = 0;
+//	}
+//	
+//	/*input current LAC 300 - 33uA/A */
+//	indication =((3300 - adc_val->CH2_ADC)/load)*30;
+//	if(indication > 0){
+//		this_obj(IND_obj_aINC)->obj_value = indication;
+//	}else{
+//		this_obj(IND_obj_aINC)->obj_value = 0;
+//	}
+//	
+//	/*output voltage DVL 1000 - 50uA/V */
+//	indication =((3300 - adc_val->CH3_ADC)/load)*20;
+//	if(indication > 0){
+//		this_obj(IND_obj_aOUTV)->obj_value = indication;
+//	}else{
+//		this_obj(IND_obj_aOUTV)->obj_value = 0;
+//	}
+//	
+//	/*output current LAC 300 - 33uA/A */
+//	indication =((3300 - adc_val->CH4_ADC)/load)*30;
+//	if(indication > 0){
+//		this_obj(IND_obj_aOUTC)->obj_value = indication;
+//	}else{
+//		this_obj(IND_obj_aOUTC)->obj_value = 0;
+//	}
+//	
+//	/*inductor voltage DVL 1000 - 50uA/V */
+//	indication =((3300 - adc_val->CH5_ADC)/load)*20;
+//	if(indication > 0){
+//		this_obj(IND_obj_aDRV)->obj_value = indication;
+//	}else{
+//		this_obj(IND_obj_aDRV)->obj_value = 0;
+//	}
 	
 //	/*inductor current LAC 300 - 33uA/A */
 //	indication =((3300 - adc_val->CH4_ADC)/load)*30;
